cb0947a161077febfc254071bb5496b5cf6ccc79
ivychill <ivychill@gmail.com>
2014-02-18 11:56:28 +0800

    update js

--- www/ZipPlugin.js
+++ www/ZipPlugin.js
@@ -1,19 +1,21 @@
-/*
- 	Author: Vishal Rajpal
- 	Filename: ZipPlugin.js
- 	Created Date: 22-01-2013
- 	Modified Date: 22-01-2013
-*/
+
+var exec = require('cordova/exec'),
+    cordova = require('cordova');
+
+function  successCallback (result) {
+    alert("SUCCESS: \r\n"+result );
+}
+
+function errorCallback (error) {
+    alert("ERROR: \r\n"+error );
+}
 
 var ExtractZipFilePlugin=function(){
 };
 
-cordova.addConstructor(function() 
+ZipPlugin.prototype.extract = function(file, successCallback, errorCallback) 
 {
-	cordova.addPlugin('ExtractZipFilePlugin', new ExtractZipFilePlugin());
-});
-
-ExtractZipFilePlugin.prototype.extractFile = function(file, successCallback, errorCallback) 
-{
-    return cordova.exec(successCallback, errorCallback, "ExtractZipFilePlugin", "extract", [file]);
+    return cordova.exec(successCallback, errorCallback, "ZipPlugin", "extract", [file]);
 };
+
+module.exports = new ZipPlugin();
